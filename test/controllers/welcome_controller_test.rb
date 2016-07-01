c7b3027f3aa2b5d997d6fc9b14081aee9ab7ab01
antoniosamuels <antoniosamuels@Antonios-iMac.local>
2016-07-01 18:52:20 -0400

    add in uploader gem

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  test "should get store" do
+    get :store
     assert_response :success
   end
 
