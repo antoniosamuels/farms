7896e4dee06656bf0e2f3f068073461b235735d0
antoniosamuels <antoniosamuels@antonios-imac.attlocal.net>
2016-07-22 22:34:16 -0400

    update

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,4 +1,4 @@
 class Farm < ActiveRecord::Base
-  has_attached_file :image, styles: { :large => "800x600#" }
+  has_attached_file :image, styles: { :small => "200x400#", :large => "700x400#" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
